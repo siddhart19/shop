15926f757ce9d29dd06132afa99f5734f727dc9a
siddhart19 <77846186+siddhart19@users.noreply.github.com>
2021-09-03 20:08:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var images = ["Screenshot_20210602-00050_WatsAppBusiness.jpeg",
+"Screenshot_20210602-000552_WatsAppBusiness.jpeg",
+ "Screenshot_20210602-000558_WatsAppBusiness.jpeg" ,
+"Screenshot_20210602-000616_WatsAppBusiness.jpeg"
+             ];
+
+             var i = 0;
+             function updatedImage()
+             {
+                 i++;
+  var number_of_images = 4
+                 if(i >number_of_images)
+                 {
+                     i = 0;
+                     var updatedImage = images[i];
+                     document.getElementById("furniture").innerHTML=updatedImage;
+                 }
+                }
